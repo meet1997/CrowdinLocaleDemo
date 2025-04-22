77b40e5832121a69d53cc1b779cb6ae3faf67345
Meet Shah <mshah@tandemdiabetes.com>
2025-04-22 11:56:53 +0530

    Project added

---
+++ CrowdinLocalizationDemo/CrowdinLocalizationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrowdinLocalizationDemoApp.swift
+//  CrowdinLocalizationDemo
+//
+//  Created by Meet Shah on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CrowdinLocalizationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
